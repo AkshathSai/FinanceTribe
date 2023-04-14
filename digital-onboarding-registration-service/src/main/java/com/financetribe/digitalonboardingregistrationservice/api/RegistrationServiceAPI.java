ba5d5813b8c2b33f4d2ed0011b9e004d9b8752d3
Akshath Sai Pittala <akshathsai.pittala@outlook.com>
2023-04-14 10:46:25 +0530

    Initial commit

--- digital-onboarding-registration-service/src/main/java/com/financetribe/digitalonboardingregistrationservice/api/RegistrationServiceAPI.java
+++ digital-onboarding-registration-service/src/main/java/com/financetribe/digitalonboardingregistrationservice/api/RegistrationServiceAPI.java
@@ -1,12 +1,13 @@
 package com.financetribe.digitalonboardingregistrationservice.api;
 
+import com.financetribe.digitalonboardingregistrationservice.model.FormData;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class RegistrationServiceAPI {
 
@@ -32,6 +33,11 @@ public class RegistrationServiceAPI {
     public String readValue() {
         //return this.environment.getProperty("spring.mail.username");
         return this.environment.getProperty("cnj.message");
+    }
+
+    @PostMapping("/register")
+    public void createUser(@RequestBody FormData formData) {
+        System.out.println(formData);
     }
 
 }
---
+++ digital-onboarding-registration-service/src/main/java/com/financetribe/digitalonboardingregistrationservice/model/FormData.java
@@ -0,0 +1,37 @@
+package com.financetribe.digitalonboardingregistrationservice.model;
+
+public class FormData {
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "name = " + name + " email = " + email + " phoneNumber = " + phoneNumber;
+    }
+}
